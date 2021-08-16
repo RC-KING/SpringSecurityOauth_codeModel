f27b07f2f980f173dea4088a80e02ac19ac07c74
jdd <2606143092@qq.com>
2021-08-16 14:33:02 +0800

    授权码模式第二次修改

--- src/main/java/com/xxxx/springsecurityoauth2demo/config/AuthorizationServerConfig.java
+++ src/main/java/com/xxxx/springsecurityoauth2demo/config/AuthorizationServerConfig.java
@@ -1,21 +1,46 @@
 package com.xxxx.springsecurityoauth2demo.config;
 
+import com.xxxx.springsecurityoauth2demo.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.oauth2.config.annotation.configurers.ClientDetailsServiceConfigurer;
 import org.springframework.security.oauth2.config.annotation.web.configuration.AuthorizationServerConfigurerAdapter;
 import org.springframework.security.oauth2.config.annotation.web.configuration.EnableAuthorizationServer;
+import org.springframework.security.oauth2.config.annotation.web.configurers.AuthorizationServerEndpointsConfigurer;
+import org.springframework.security.oauth2.provider.token.TokenStore;
 
 
 /**
  * 授权服务器的配置
+ * 使用密码模式
  */
 @Configuration
 @EnableAuthorizationServer
 public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdapter {
+
     @Autowired
     private PasswordEncoder passwordEncoder;
+
+
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    @Autowired
+    private UserService userService;
+
+    @Autowired
+    @Qualifier("redisTokenStore")
+    private TokenStore tokenStore;
+
+    @Override
+    public void configure(AuthorizationServerEndpointsConfigurer endpoints) throws Exception {
+        endpoints.authenticationManager(authenticationManager)
+                .userDetailsService(userService)
+                .tokenStore(tokenStore);
+    }
 
     @Override
     public void configure(ClientDetailsServiceConfigurer clients) throws Exception {
@@ -34,6 +59,6 @@ public class AuthorizationServerConfig extends AuthorizationServerConfigurerAdap
                 // 配置申请的权限范围
                 .scopes("all")
                 // 配置grant_type,表示授权类型
-                .authorizedGrantTypes("authorization_code");
+                .authorizedGrantTypes("password");
     }
 }
